b3ab59ccf37990862e08f233ebbcdb5b08639130
John Carney <john@pdfta.com>
2020-01-01 15:59:09 +0000

    Can show a basic blog page generated from markdown.

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from flask_flatpages import FlatPages
 
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+pages = FlatPages(app)
 
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+
+@app.route("/<path:path>/")
+def page(path):
+    page = pages.get_or_404(path)
+    return render_template("page.html", page=page)
 
 
 if __name__ == "__main__":
---
+++ pages/example-text.md
@@ -0,0 +1,5 @@
+title: My First Entry
+
+date: 2020-01-01
+
+This is my **first** blog post ever! Welcome!
